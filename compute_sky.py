19d7b7107ee6a8064409959f3dfbffefedd2d200
Nathan Miles <nmiles@stsci.edu>
2019-12-15 16:49:01 -0500

    adding new lambda function for computing sky backgrounds

---
+++ compute_sky.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import glob
+
+from astropy.table import Table
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
+from .fits_handler import FitsHandler
+# from acstools import calacs
+import boto3
+
+
+def download_file(event):
+    fname = event['fits_s3_key']
+    bucket_name = event['fits_s3_bucket']
+    basename = fname.split('/')[-1].split('_')[0]
+
+    s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')
+    bkt = s3.Bucket(bucket_name)
+    bkt.download_file(fname, f"/tmp/{basename}",
+                      ExtraArgs={"RequestPayer": "requester"})
+    return f"/tmp/{basename}"
+
+def get_image_metadata(fitsobj):
+    metadata = {}
+    keys = ['targname', 'exptime', 'filter1','filter2', 'expstart']
+    for key in keys:
+        metadata[key] = fitsobj.prhdr[key]
+    return metadata
+
+def process_event(event):
+    fname = download_file(event)
+    basename = fname.split('/')[-1]
+    fitsobj = FitsHandler(fname=fname)
+    fitsobj.get_data(ext='sci')
+    fitsobj.get_data(ext='dq')
+    metadata = get_image_metadata(fitsobj)
+    units = fitsobj.chip1['sci1_hdr']['BUNIT']
+
+    med = 0
+    for chip in [fitsobj.chip1, fitsobj.chip2]:
+        _, chip_med, _ = sigma_clipped_stats(
+            chip['sci'][chip['dq']==0],
+            sigma=5,
+            maxiters=5
+        )
+        med += chip_med
+
+    avg_bkg = med/2.0
+    metadata[f"bkg_{units}"] = avg_bkg
+    tb = Table(metadata)
+    tb.write(f"/tmp/{basename}_sky.dat", format='ascii')
+
+
+def handler(event, context):
+    print(event['s3_output_bucket'])
+    print(event['fits_s3_key'])
+    print(event['fits_s3_bucket'])
+    process_event(event)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ fits_handler.py
@@ -0,0 +1,116 @@
+import logging
+
+from astropy.io import fits
+import numpy as np
+
+
+logging.basicConfig(format='%(levelname)-4s '
+                           '[%(module)s.%(funcName)s:%(lineno)d]'
+                           ' %(message)s',
+                    )
+LOG = logging.getLogger('fits_handler')
+LOG.setLevel(logging.INFO)
+
+class FitsHandler(object):
+    def __init__(self, fname=None):
+
+        self._fname = fname
+        self._prhdr = None
+
+        # Container for info from both chips
+        self._chip1= {
+            'sci': None,
+            'err': None,
+            'dq': None,
+        }
+
+        self._chip2= {
+            'sci': None,
+            'err': None,
+            'dq': None,
+        }
+        # self._chips = [self._chip2, self._chip1]
+
+    @property
+    def fname(self):
+        """Filename of the FITS image"""
+        return self._fname
+
+    @fname.setter
+    def fname(self, value):
+        self._fname = value
+
+    @property
+    def prhdr(self):
+        """Primary Header of the FITS image"""
+        return self._prhdr
+
+    @prhdr.setter
+    def prhdr(self, value):
+        self._prhdr = value
+
+    @property
+    def chip1(self):
+        """Container for storing data for both chips
+
+        A dict with the following attirbutes:
+
+            * sci
+            * err
+            * dq
+
+
+        """
+        return self._chip1
+
+    @chip1.setter
+    def chip1(self, value):
+        self._chip1 = value
+
+    @property
+    def chip2(self):
+        """Container for storing data for both chips
+
+        A dict with the following attirbutes:
+
+            * sci
+            * err
+            * dq
+
+
+        """
+        return self._chip2
+
+    @chip2.setter
+    def chip2(self, value):
+        self._chip2 = value
+
+    def get_data(self, ext='dq'):
+        """ Grab the data from the specified extension of the FITS file
+
+        Parameters
+        ----------
+        ext : str
+            The name of the extension (`sci`, `err`, `dq`).
+
+        Returns
+        -------
+
+        """
+        ext1 = (ext, 1)  # Chip 2
+        ext2 = (ext, 2)  # Chip 1
+        self._prhdr = fits.getheader(self.fname, 0)
+        i = 2
+        for ext_tuple, chip in zip([ext1, ext2], [self.chip2, self.chip1]):
+            with fits.open(self.fname) as hdu:
+                try:
+                    ext_idx = hdu.index_of(ext_tuple)
+                    ext_data = hdu[ext_idx].data
+                except KeyError:
+                    LOG.info(
+                        '{1} is missing for {0}'.format(self._fname, ext1))
+                else:
+                    chip[ext] = ext_data
+                    chip[f"{ext}{i}_hdr"] = hdu[ext_idx].header
+                finally:
+                    i -=1
